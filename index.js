a1ff5d3478a032a7e2ae52c1dfa3959a365f738e
GONADIC <jdm7282@gmail.com>
2020-09-11 20:26:41 +0900

    add comnmmit

--- index.js
+++ index.js
@@ -1,243 +1,59 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.argv.length == 2 ? process.env.token : "";
-const welcomeChannelName = "ㅎㅇㅎㅇ";
-const byeChannelName = "ㅂㅇㅂㅇ";
-const welcomeChannelComment = "ㅎㅇㅎㅇ.";
-const byeChannelComment = "ㅂㅇㅂㅇ";
 
-client.on('ready', () => {
-  console.log('켰다.');
-  client.user.setPresence({ game: { name: '해킹샵 오픈준비중..' }, status: 'online' })
-});
+@client.event
+async def on_ready():
+    print("인증 봇이 정상적으로 실행되었습니다.")
+    game = discord.Game('PLAYSOME샵 준비중')
+    await client.change_presence(status=discord.Status.online, activity=game)
 
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  const newUser = member.user;
-  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+@client.event
+async def on_message(message):
+    if message.content.startswith("/인증"): #명령어 /인증
+        a = ""
+        Captcha_img = ImageCaptcha()
+        for i in range(6):
+            a += str(random.randint(0, 9))
 
-  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+        name = str(message.author.id) + ".png"
+        Captcha_img.write(a, name)
 
-  member.addRole(guild.roles.find(role => role.name == "게스트"));
-});
+        await message.channel.send(f"""{message.author.mention} 아래 숫자를 10초 내에 입력해주세요. """)
+        await message.channel.send(file=discord.File(name))
 
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  const deleteUser = member.user;
-  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+        def check(msg):
+            return msg.author == message.author and msg.channel == message.channel
 
-  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
-});
+        try:
+            msg = await client.wait_for("message", timeout=10, check=check) # 제한시간 10초
+        except:
+            await message.channel.purge(limit=3)
+            chrhkEmbed = discord.Embed(title='❌ 인증실패', color=0xFF0000)
+            chrhkEmbed.add_field(name='닉네임', value=message.author, inline=False)
+            chrhkEmbed.add_field(name='이유', value='시간초과', inline=False)
+            await message.channel.send(embed=chrhkEmbed)
+            print(f'{message.author} 님이 시간초과로 인해 인증을 실패함.')
+            return
 
-client.on('message', (message) => {
-  if(message.author.bot) return;
+        if msg.content == a:
+            role = discord.utils.get(message.guild.roles, name="★★역 = 할 = 이 = 름★★")
+            await message.channel.purge(limit=4)
+            tjdrhdEmbed = discord.Embed(title='인증성공', color=0x04FF00)
+            tjdrhdEmbed.add_field(name='닉네임', value=message.author, inline=False)
+            tjdrhdEmbed.add_field(name='5초후 인증역할이 부여됩니다.', value='** **', inline=False)
+            tjdrhdEmbed.set_thumbnail(url=message.author.avatar_url)
+            await message.channel.send(embed=tjdrhdEmbed)
+            time.sleep(5)
+            await message.author.add_roles(role)
+        else:
+            await message.channel.purge(limit=4)
+            tlfvoEmbed = discord.Embed(title='❌ 인증실패', color=0xFF0000)
+            tlfvoEmbed.add_field(name='닉네임', value=message.author, inline=False)
+            tlfvoEmbed.add_field(name='이유', value='잘못된 숫자', inline=False)
+            await message.channel.send(embed=tlfvoEmbed)
+            print(f'{message.author} 님이 잘못된 숫자로 인해 인증을 실패함.')
 
-  if(message.content == 'ping') {
-    return message.reply('pong pong pong pong pong 퐁 퐁 퐁 퐁');
-  }
-
-  if(message.content == 'embed') {
-    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
-    let embed = new Discord.RichEmbed()
-      .setTitle('타이틀')
-      .setURL('http://www.naver.com')
-      .setAuthor('고나딕', img, 'http://www.naver.com')
-      .setThumbnail(img)
-      .addBlankField()
-      .addField('Inline field title', 'Some value here')
-      .addField('Inline field title', 'Some value here', true)
-      .addField('Inline field title', 'Some value here', true)
-      .addField('Inline field title', 'Some value here', true)
-      .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
-      .addBlankField()
-      .setTimestamp()
-      .setFooter('고나딕 만듬', img)
-
-    message.channel.send(embed)
-  } else if(message.content == '!hel') {
-    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
-    let commandList = [
-      {name: '!help', desc: 'help'},
-      {name: 'ping', desc: '현재 핑 상태'},
-      {name: 'embed', desc: 'embed 예제1'},
-      {name: '!전체공지', desc: 'dm으로 전체 공지 보내기'},
-      {name: '!전체공지2', desc: 'dm으로 전체 embed 형식으로 공지 보내기'},
-      {name: '!청소', desc: '텍스트 지움'},
-      {name: '!초대코드', desc: '해당 채널의 초대 코드 표기'},
-      {name: '!초대코드2', desc: '봇이 들어가있는 모든 채널의 초대 코드 표기'},
-    ];
-    let commandStr = '';
-    let embed = new Discord.RichEmbed()
-      .setAuthor('고나딕', helpImg)
-      .setColor('#186de6')
-      .setFooter(`고나딕`)
-      .setTimestamp()
-    
-    commandList.forEach(x => {
-      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
-    });
-
-    embed.addField('Commands: ', commandStr);
-
-    message.channel.send(embed)
-  } else if(message.content == '!초대코드2') {
-    client.guilds.array().forEach(x => {
-      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-        .then(invite => {
-          message.channel.send(invite.url)
-        })
-        .catch((err) => {
-          if(err.code == 50013) {
-            message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
-          }
-        })
-    });
-  } else if(message.content == '!초대코드') {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-    message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-      .then(invite => {
-        message.channel.send(invite.url)
-      })
-      .catch((err) => {
-        if(err.code == 50013) {
-          message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
-        }
-      })
-  } else if(message.content.startsWith('!공지')) {
-    if(checkPermission(message)) return
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!공지'.length);
-      let embed = new Discord.RichEmbed()
-        .setAuthor('해킹샵 오픈준비중..', 'https://cdn.discordapp.com/icons/742910703274426409/15f6fb9d663813091c45f0fc8a89bfd6.webp?size=128')
-        .setColor('#18e6a1')
-        .setFooter(`이 메시지는 𝗧𝗘𝗦𝗧 𝗛𝗔𝗖𝗞𝗜𝗡𝗚샵 에서 발송 했습니다`)
-        .setImage('https://cdn.discordapp.com/avatars/357138746245644289/a_9c68210dce67e15bda118fb1da7a8fd5.gif?size=128')
-        
-  
-      embed.addField('@everyone ', contents);
-  
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(embed)
-      });
-  
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith('!공지2')) {
-    if(checkPermission(message)) return
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!공지2'.length);
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(`<@${message.author.id}> ${contents}`);
-      });
-  
-      return message.reply('전송완료 해쬬용!');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith('!청소')) {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-    
-    if(message.channel.type != 'dm' && checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) { // c @나긋해 3
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        let _cnt = 0;
-
-        message.channel.fetchMessages().then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
-        });
-      }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
-    }
-  }
-});
-
-function checkPermission(message) {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.channel.send(`<@${message.author.id}> ` + "흠흠.. 하기싫은데....")
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function changeCommandStringLength(str, limitLen = 8) {
-  let tmp = str;
-  limitLen -= tmp.length;
-
-  for(let i=0;i<limitLen;i++) {
-      tmp += ' ';
-  }
-
-  return tmp;
-}
-
-async function AutoMsgDelete(message, str, delay = 3000) {
-  let msg = await message.channel.send(str);
-
-  setTimeout(() => {
-    msg.delete();
-  }, delay);
-}
-
-const { bot } = require('../index');
-const Discord = require("discord.js");
-require('date-utils').language("kr");
-
-const serverStats = {
-    guildID: '747729946893484112',//서버방 ID
-    memberCounID: '747808704492994632',//음성방 ID
-};
-
-bot.on("guildMemberAdd", member => {
-    //입장
-    let channel = member.guild.channels.find(join => join.name === ':bell:：안녕히가세요');
-    if (!channel) return;
-    let Join = new Discord.RichEmbed()
-    .setColor("#04FF00")
-    .setDescription(member.user + '님이 접속하셧습니다.')
-    .setFooter('샵에 오신걸 환영합니다 .');
-    channel.send(Join);
+client.run('')
 
 
-    //서버정보
-    if(member.guild.id !== serverStats.guildID) return;
-    bot.channels.get(serverStats.memberCounID).setName(인원수 : ${member.guild.members.filter(m => !m.user.bot).size});
-
-    // 자동 역할 다른거 다 같고  입장시 줄 역할
-    var role = member.guild.roles.find(r => r.name === "비회원");
-    member.addRole(role);
-
-});
-
-
-client.login(token);
