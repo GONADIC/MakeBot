4175df76a1de0873eb2a2e0e45bdd757557176fe
GONADIC <jdm7282@gmail.com>
2020-09-16 15:53:52 +0900

    add vcovvv

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ client.on('message', (message) => {
         .setAuthor('! MELT#7777', 'https://cdn.discordapp.com/attachments/755601938657050624/755647640393482250/e6a099941a07707ae93b82bd70aa4c82.webp')
         .setColor('#ff9999')
         .setFooter(`이 메시지는 𝗠𝗘𝗟𝗧 샵에서 발송 했습니다`)
-        .setImage('https://cdn.discordapp.com/attachments/755601938657050624/755682097561075812/ezgif-1-e5b62251d051.gif')
+        .setImage('https://cdn.discordapp.com/attachments/755601938657050624/755682681504923668/ezgif-1-09af1203224e.gif')
         
   
       embed.addField('@everyone ', contents);
